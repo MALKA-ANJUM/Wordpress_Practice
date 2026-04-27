1047e788a6b411e82619f4979590d1c4bdd000c0
Malka Anjum <malkaanjum1999@gmail.com>
2026-04-27 20:17:09 +0530

    first plugin added

---
+++ wp-content/plugins/sanabil-plugin/sanabil-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Plugin Name:       Sanabil Plugin
+ * Plugin URI:        http://wordpress_practice.test/
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Malka Anjum
+ * Author URI:        http://wordpress_practice.test/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       sanabil-plugin
+ */
